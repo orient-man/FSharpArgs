be0df400be1bfd81434eb70199568850957f97c5
orientman <orientman@gmail.com>
2014-10-03 16:41:04 +0200

    Multiple bool switches

--- Args.fs
+++ Args.fs
@@ -9,9 +9,20 @@ type ParsingResult(valuesIn: Map<char, obj>) =
             | None -> false
 
 let Parse schema args =
+    let validArgs = schema |> Set.ofSeq
+
+    let FindElements (arg: string) =
+        if arg.StartsWith "-"
+        then arg |> Seq.skip 1
+        else Seq.empty
+
     let values =
-        schema
-        |> Seq.map (fun s -> (s, box true))
+        args
+        |> Seq.ofList
+        |> Seq.map FindElements
+        |> Seq.concat
+        |> Seq.where validArgs.Contains
+        |> Seq.map (fun c -> (c, box true))
         |> Map.ofSeq
 
     ParsingResult values
--- ArgsTests.fs
+++ ArgsTests.fs
@@ -4,10 +4,21 @@ open Args
 
 [<Test>]
 let ``Parses with no schema or arguments``() =
-    let result = Parse "" Array.empty<string>
+    let result = Parse "" []
     Assert.AreEqual(0, result.Cordinality)
 
 [<Test>]
 let ``Simple bool present``() =
     let result = Parse "x" ["-x"]
     Assert.IsTrue(result.GetBool 'x')
+
+[<Test>]
+let ``Multiple bool arguments``() =
+    let result = Parse "xy" ["-xy"]
+    Assert.IsTrue(result.GetBool 'x')
+    Assert.IsTrue(result.GetBool 'y')
+
+[<Test>]
+let ``Simple bool not present``() =
+    let result = Parse "x" []
+    Assert.IsFalse(result.GetBool 'x')
