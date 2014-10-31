a7af24dee8fa03c85fe7104e32a7739d7c578e0c
orientman <orientman@gmail.com>
2014-10-31 17:35:48 +0100

    List.map + Tree.fold (last one is hard!)

--- Playground.fs
+++ Playground.fs
@@ -13,3 +13,25 @@ let alltrue = foldr (&&) true
 let cons x y = x::y
 let copy list = foldr cons [] list
 let append a b = foldr cons b a
+let count a n = n + 1
+let length list = foldr count 0 list
+let double = (*) 2
+//let fandcons f = f >> cons
+//let doubleandcons = fandcons double
+//let doubleall = foldr doubleandcons []
+//let doubleall = foldr (double >> cons) []
+let map f = foldr (f >> cons) []
+let doubleall = map double
+let summatrix = map sum >> sum
+
+type Tree<'a> = | Node of 'a * Tree<'a> list
+
+let rec foldtree f g a = function
+    | Node(label, subtrees) ->
+        f label (foldchildren f g a subtrees)
+and foldchildren f g a = function
+    | [] -> a
+    | subtree::rest ->
+        g (foldtree f g a subtree) (foldchildren f g a rest)
+
+let sumtree = foldtree (+) (+) 0
--- PlaygroundTests.fs
+++ PlaygroundTests.fs
@@ -13,3 +13,8 @@ let ``all that stuff just works``() =
     alltrue [true;true] |> should be True
     copy [1;2;3] |> should equal [1;2;3]
     append [1;2] [3;4] |> should equal [1;2;3;4]
+    length [1;2;3] |> should equal 3
+    doubleall [1;2;3] |> should equal [2;4;6]
+    summatrix [[1;2];[3;4]] |> should equal 10
+    sumtree (Node(1, [Node(2, []); Node(3, [Node(4, [])])])) |> should equal 10
+    sumtree (Node(1, Node(2, [])::Node(3, Node(4, [])::[])::[])) |> should equal 10
