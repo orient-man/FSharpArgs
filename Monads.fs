28b2c2781d1f5ec6626e501d73363dae273a2e80
orientman <orientman@gmail.com>
2014-11-03 15:04:38 +0100

    Monads from: http://mikehadlow.blogspot.co.uk/2011/01/monads-in-c1-introduction.html

---
+++ Monads.fs
@@ -0,0 +1,39 @@
+﻿module Monads
+
+let (>>=) x f = fun s0 -> let a, s1 = x s0 in f a s1
+
+let returnS a = fun s -> a, s
+
+type StateBuilder() =
+    member m.Bind(x, f) = x >>= f
+    member m.Return a = returnS a
+
+let getState = fun s -> s, s
+
+let setState s = fun _ -> (), s
+
+let Execute m s = m s |> fst
+
+let (>.>) a b = a >>= fun _ -> b
+
+let NextShort state =
+    let newState = (214013 * state) + 2531011
+    let rand = int16 ((newState &&& System.Int32.MaxValue) >>> 16)
+    (rand, newState)
+
+let getRandom =
+    getState >>=
+        (fun s0 ->
+            let x, s1 = NextShort s0
+            in setState s1 >.> (returnS x))
+
+let resultMonadic =
+    let state = new StateBuilder()
+    let m = state {
+        let! a = getRandom
+        let! b = getRandom
+        let! c = getRandom
+        return a + b + c
+    }
+    let seed = 0
+    Execute m seed
---
+++ MonadsTests.fs
@@ -0,0 +1,14 @@
+﻿module MonadsTests
+open Monads
+open FsUnit
+open NUnit.Framework
+
+let expectedResult = 28995
+
+[<Test>]
+let ``all that stuff just works``() =
+    let a, s1 = NextShort 0
+    let b, s2 = NextShort s1
+    let c, _ = NextShort s2
+    (a + b + c) |> should equal expectedResult
+    resultMonadic |> should equal expectedResult
