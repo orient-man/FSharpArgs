d28fca9d9afc2bbe85c7ddae43a15fd32af850d4
orientman <orientman@gmail.com>
2014-11-03 15:39:31 +0100

    Snd version

--- Monads.fs
+++ Monads.fs
@@ -1,39 +1,40 @@
 ﻿module Monads
 
-let (>>=) x f = fun s0 -> let a, s1 = x s0 in f a s1
+type State<'a, 's> = State of ('s -> 'a * 's)
 
-let returnS a = fun s -> a, s
+let runState (State s) a = s a
+let getState = State (fun s -> (s,s))
+let putState s = State (fun _ -> ((),s))
 
 type StateBuilder() =
-    member m.Bind(x, f) = x >>= f
-    member m.Return a = returnS a
+    member this.Return(a) = State (fun s -> (a,s))
+    member this.Bind(m,k) =
+        State (fun s ->
+            let (a,s') = runState m s in runState (k a) s')
+    member this.ReturnFrom (m) = m
 
-let getState = fun s -> s, s
+let state = new StateBuilder()
 
-let setState s = fun _ -> (), s
-
-let Execute m s = m s |> fst
-
-let (>.>) a b = a >>= fun _ -> b
+let lift f =
+    state {
+        let! s = getState
+        return! putState (f s)
+    }
 
 let NextShort state =
     let newState = (214013 * state) + 2531011
     let rand = int16 ((newState &&& System.Int32.MaxValue) >>> 16)
     (rand, newState)
 
-let getRandom =
-    getState >>=
-        (fun s0 ->
-            let x, s1 = NextShort s0
-            in setState s1 >.> (returnS x))
+let AggregateRandom (acc, s0) =
+    let cur, s1 = NextShort s0 in (acc + cur, s1)
 
 let resultMonadic =
-    let state = new StateBuilder()
-    let m = state {
-        let! a = getRandom
-        let! b = getRandom
-        let! c = getRandom
-        return a + b + c
+    let AggregateRandom' = lift AggregateRandom
+    let s = state {
+        do! AggregateRandom'
+        do! AggregateRandom'
+        do! AggregateRandom'
     }
     let seed = 0
-    Execute m seed
+    runState s (int16 0, seed) |> snd |> fst
--- MonadsTests.fs
+++ MonadsTests.fs
@@ -3,12 +3,15 @@ open Monads
 open FsUnit
 open NUnit.Framework
 
-let expectedResult = 28995
-
 [<Test>]
 let ``all that stuff just works``() =
     let a, s1 = NextShort 0
     let b, s2 = NextShort s1
     let c, _ = NextShort s2
-    (a + b + c) |> should equal expectedResult
-    resultMonadic |> should equal expectedResult
+    resultMonadic |> should equal (a + b + c)
+    (int16 0, 0)
+        |> AggregateRandom
+        |> AggregateRandom
+        |> AggregateRandom
+        |> fst
+        |> should equal (a + b + c)
